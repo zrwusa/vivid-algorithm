0fdcd037d2300eb83010be861eaa27be072ca00f
Revone <zrwusa@gmail.com>
2023-09-19 02:32:35 +0800

    [data-structure-typed] v1.3.2 published

---
+++ src/algorithms/queue/queue.ts
@@ -0,0 +1,81 @@
+import {runAlgorithm} from '../helpers';
+import {LinkedListQueue, Queue} from 'data-structure-typed';
+
+const magnitude = 10000;
+export async function testLinkedLIstQueueFIFO() {
+  const lQueue = new LinkedListQueue<number>();
+  for (let i = 0; i < magnitude; i++) {
+    lQueue.enqueue(i);
+  }
+  for (let i = 0; i < magnitude; i++) {
+    lQueue.dequeue();
+  }
+}
+
+export async function testLinkedLIstQueueAccess() {
+  const lQueue = new LinkedListQueue<number>();
+  for (let i = 0; i < magnitude; i++) {
+    lQueue.enqueue(i);
+  }
+  let val: number | undefined = 0;
+  for (let i = 0; i < magnitude; i++) {
+    val = lQueue.getAt(i);
+    // console.log(val);
+  }
+  return val;
+}
+
+export async function testQueueFIFO() {
+  const queue = new Queue<number>();
+  for (let i = 0; i < magnitude; i++) {
+    queue.enqueue(i);
+  }
+  for (let i = 0; i < magnitude; i++) {
+    queue.dequeue();
+  }
+}
+
+export async function testQueueAccess() {
+  const queue = new Queue<number>();
+  for (let i = 0; i < magnitude; i++) {
+    queue.enqueue(i);
+  }
+  let val: number | undefined = 0;
+  for (let i = 0; i < magnitude; i++) {
+    val = queue.getAt(i);
+    // console.log(val);
+  }
+  return val;
+}
+
+export async function testNativeArrayFIFO() {
+  const nQueue = new Array<number>();
+  for (let i = 0; i < magnitude; i++) {
+    nQueue.push(i);
+  }
+  for (let i = 0; i < magnitude; i++) {
+    nQueue.shift();
+  }
+}
+
+export async function testNativeArrayAccess() {
+  const nQueue = new Array<number>();
+  for (let i = 0; i < magnitude; i++) {
+    nQueue.push(i);
+  }
+  let val: number | undefined = 0;
+  for (let i = 0; i < magnitude; i++) {
+    val = nQueue[i];
+    // console.log(val);
+  }
+  return val;
+}
+
+export const runTestQueue = async () => {
+  await runAlgorithm(testLinkedLIstQueueFIFO, true, []);
+  await runAlgorithm(testQueueFIFO, true, []);
+  await runAlgorithm(testNativeArrayFIFO, true, []);
+  await runAlgorithm(testLinkedLIstQueueAccess, true, []);
+  await runAlgorithm(testQueueAccess, true, []);
+  await runAlgorithm(testNativeArrayAccess, true, []);
+}
--- src/pages/algorithm/queue-deque/index.tsx
+++ src/pages/algorithm/queue-deque/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import {runAllMaxSlidingWindow} from '../../../algorithms/deque';
+import {runTestQueue} from '../../../algorithms/queue';
 
 export const QueueDequeScreen = () => {
   return <>
     <Button onClick={() => {
       runAllMaxSlidingWindow().then()
     }}>Max Sliding Window</Button>
+    <Button onClick={() => {
+      runTestQueue().then()
+    }}>Test Queue</Button>
   </>
 }
 
